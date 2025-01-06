e3ff22716e34adfeb0260b894db0ba8c546507d7
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2025-01-06 15:29:02 +0530

    Add files via upload

---
+++ Stranger/plugins/helpers.py
@@ -0,0 +1,158 @@
+
+import re
+import asyncio
+import math
+import os
+import time
+import json
+from datetime import datetime as dt
+from pyrogram.errors import FloodWait, InviteHashInvalid, InviteHashExpired, UserAlreadyParticipant
+from pyrogram import Client
+#Join private chat-------------------------------------------------------------------------------------------------------------
+
+async def join(client:Client, invite_link):
+    try:
+        await client.join_chat(invite_link)
+        return "Successfully joined the Channel"
+    except UserAlreadyParticipant:
+        return "User is already a participant."
+    except (InviteHashInvalid, InviteHashExpired):
+        return "Could not join. Maybe your link is expired or Invalid."
+    except FloodWait:
+        return "Too many requests, try again later."
+    except Exception as e:
+        print(e)
+        return "Could not join, try joining manually."
+    
+#Regex---------------------------------------------------------------------------------------------------------------
+#to get the url from event
+
+def get_link(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)   
+    try:
+        link = [x[0] for x in url][0]
+        if link:
+            return link
+        else:
+            return False
+    except Exception:
+        return False
+    
+#Screenshot---------------------------------------------------------------------------------------------------------------
+
+def hhmmss(seconds):
+    x = time.strftime('%H:%M:%S',time.gmtime(seconds))
+    return x
+
+async def screenshot(video, duration, sender):
+    if os.path.exists(f'{sender}.jpg'):
+        return f'{sender}.jpg'
+    time_stamp = hhmmss(int(duration)/2)
+    out = dt.now().isoformat("_", "seconds") + ".jpg"
+    cmd = ["ffmpeg",
+           "-ss",
+           f"{time_stamp}", 
+           "-i",
+           f"{video}",
+           "-frames:v",
+           "1", 
+           f"{out}",
+           "-y"
+          ]
+    process = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    x = stderr.decode().strip()
+    y = stdout.decode().strip()
+    if os.path.isfile(out):
+        return out
+    else:
+        None       
+
+FINISHED_PROGRESS_STR = "█"
+UN_FINISHED_PROGRESS_STR = ""
+DOWNLOAD_LOCATION = "/app"
+
+
+async def progress_for_pyrogram(
+    current,
+    total,
+    bot,
+    ud_type,
+    message,
+    start
+):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        status = DOWNLOAD_LOCATION + "/status.json"
+        if os.path.exists(status):
+            with open(status, 'r+') as f:
+                statusMsg = json.load(f)
+                if not statusMsg["running"]:
+                    bot.stop_transmission()
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "**[{0}{1}]** `| {2}%`\n\n".format(
+            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
+            ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+
+        tmp = progress + "GROSSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            if not message.photo:
+                await message.edit_text(
+                    text="{}\n {}".format(
+                        ud_type,
+                        tmp
+                    )
+                )
+            else:
+                await message.edit_caption(
+                    caption="{}\n {}".format(
+                        ud_type,
+                        tmp
+                    )
+                )
+        except:
+            pass
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "")
+    return tmp[:-2]
---
+++ Stranger/plugins/start.py
@@ -0,0 +1,204 @@
+import re
+import os
+import time
+import asyncio
+
+from pyromod.exceptions import ListenerTimeout
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.errors import ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid, FloodWait
+from pyrogram.enums import MessageMediaType
+from Stranger import app , OWNER_ID, userbot , LOGGER_ID
+from Stranger.plugins.helpers import get_link, progress_for_pyrogram
+
+queue = []
+pattern = "https://t.me/(?:c/)?(.*)/(\d+)"
+
+async def clone(sender , s_channel_id, des_channel_id , i, channel_id ,xx):
+    try:
+        await xx.edit("Cloning...")
+        msg = await userbot.get_messages(s_channel_id , i)
+        file = await userbot.download_media(
+            msg,
+            progress=progress_for_pyrogram,
+            progress_args=(
+                        app,
+                        "**DOWNLOADING:**\n",
+                        xx,
+                        time.time()
+                    ),
+            in_memory=True
+            )
+        await xx.edit('Preparing to Upload!')
+        caption = None
+        if msg.caption is not None:
+            caption = msg.caption
+        if msg.media==MessageMediaType.VIDEO_NOTE:              
+            await userbot.send_video_note(
+                chat_id=des_channel_id,
+                video_note=file,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                            app,
+                            '**UPLOADING:**\n',
+                            xx,
+                            time.time()
+                        )
+            )
+        elif msg.media==MessageMediaType.VIDEO and msg.video.mime_type in ["video/mp4", "video/x-matroska"]:
+            await userbot.send_video(
+                chat_id=des_channel_id,
+                video=file,
+                caption=caption,
+                supports_streaming=True,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                            app,
+                            '**UPLOADING:**\n',
+                            xx,
+                            time.time()
+                        )
+            )
+        elif msg.media==MessageMediaType.PHOTO:
+            await userbot.send_photo(des_channel_id, file, caption=caption)
+        else:
+            await userbot.send_document(
+                des_channel_id,
+                file, 
+                caption=caption,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                            app,
+                            '**UPLOADING:**\n',
+                            xx,
+                            time.time()
+                        )
+            )
+        try:
+            os.remove(file)
+            if os.path.isfile(file) == True:
+                os.remove(file)
+        except Exception:
+            pass
+
+        await app.send_message(LOGGER_ID,"https://t.me/c/{}/{}".format(channel_id,i))
+    except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
+        await app.send_message(sender, "Have you joined the channel?")
+        return
+    except Exception as e:
+        print(e)
+        return
+
+async def run_batch(sender ,d, s, start_msg_id, end_msg_id, channel_id):
+    yy = await app.send_message(sender,".")
+    xx = await app.send_message(sender,"Starting...")
+    total = end_msg_id-start_msg_id
+    for i in range(start_msg_id,end_msg_id+1):
+        await yy.edit(f"Sending message {i-start_msg_id + 1} of {total}...")
+        try: 
+            if not sender in queue:
+                await app.send_message(sender, "Batch completed.")
+                break
+        except Exception as e:
+            print(e)
+            await app.send_message(sender, "Batch completed.")
+            break
+        try:
+            await clone(sender , s, d, i, channel_id, xx)
+        except FloodWait as fw:
+            await app.send_message(sender, "Floodwait {}".format(fw.value))
+            await asyncio.sleep(fw.value + 5)
+            await clone(sender ,s, d, i, channel_id, xx)
+        await asyncio.sleep(1)
+        
+
+@app.on_message(filters.command("cancel") & filters.private & filters.user(OWNER_ID))
+async def cancel(client, message:Message):
+    if not message.from_user.id in queue:
+        return await message.reply_text("No tasks to cancel.")
+    queue.clear()
+    await message.reply_text("Tasks cancelled.")
+
+@app.on_message(filters.command("start") & filters.private & filters.user(OWNER_ID))
+async def batch(client, message:Message):
+    sender = message.from_user.id
+    s_channel_id =None
+    if sender in queue:
+        return await message.reply_text("You already have a batch running.")
+    chat = message.chat
+    while True:
+        try:
+            response = await chat.ask("Send me the destination channel link", timeout=60)
+        except ListenerTimeout:
+            return await message.reply_text("No response received.")
+        link = response.text
+        if not link:
+            await response.reply("Not a valid link found. Try again")
+            continue
+        try:
+            des = await userbot.get_chat(link)
+            des_channel_id = des.id
+            break
+        except Exception as e:
+            print(e)
+            await response.reply("Not a valid link found. Try again")
+            continue
+
+    while True:
+        try:
+            response = await chat.ask("Send me the message link you want to start saving from", timeout=60)
+        except ListenerTimeout:
+            return await message.reply_text("No response received.")
+  
+        try:
+            matches = re.match(pattern, response.text)
+            if not matches:
+                await response.reply("Not a valid link found. Try again")
+                continue
+            channel_id = matches.group(1)
+            s_channel_id= int(f"-100{channel_id}")
+            start_msg_id = int(matches.group(2))
+            break
+        except Exception as e:
+            print(e)
+            await response.reply("Not a valid link found. Try again")
+            continue
+    
+    while True:
+        try:
+            response = await chat.ask("Send me the message link until where you want to save", timeout=60)
+        except ListenerTimeout:
+            return await message.reply_text("No response received.")
+        
+        try:
+            d_link = get_link(response.text)
+            if not d_link:
+                await response.reply("Not a valid link found. Try again")
+                continue
+            matches = re.match(pattern, response.text)
+            if not matches:
+                await response.reply("Not a valid link found. Try again")
+                continue
+            channel_id = matches.group(1)
+            if s_channel_id != int(f"-100{channel_id}"):
+                await response.reply("Ending msg link is not from the same source channel . Try again")
+                continue
+            end_msg_id = int(matches.group(2))
+            break
+        except Exception as e:
+            print(e)
+            await message.reply_text("No link found.")
+            break
+
+    queue.append(sender)
+    await app.send_message(LOGGER_ID,"Batch started. Messages will be saved between {} and {}".format(s_channel_id,des_channel_id))
+    if start_msg_id == end_msg_id:
+        return await message.reply_text("start msg id and end msg id can't be same. Cancelling the process")
+    elif start_msg_id > end_msg_id:
+        x = start_msg_id
+        start_msg_id = end_msg_id
+        end_msg_id = x
+    await run_batch(sender ,des_channel_id, s_channel_id, start_msg_id, end_msg_id ,channel_id)
+    queue.clear()
+
+
